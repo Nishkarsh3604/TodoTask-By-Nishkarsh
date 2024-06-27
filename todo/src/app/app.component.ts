393b6f0cca364c7bb89173914db582f15ffc2b93
Nishkarsh3604 <141808203+Nishkarsh3604@users.noreply.github.com>
2024-06-28 03:50:27 +0530

    Update app.component.ts

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -16,7 +16,7 @@ interface Task {
   standalone: true,
   imports: [RouterOutlet, FormsModule, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // Corrected styleUrls
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent implements OnInit {
   title = 'todo';
